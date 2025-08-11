926384088ad783e1667e4e23eeadfdb9c47f12c2
kyle-barco <kylebarco04@gmail.com>
2025-08-11 14:00:57 +0800

    fix ssl

--- src/01_models/pool.cjs
+++ src/01_models/pool.cjs
@@ -3,6 +3,6 @@ require("dotenv").config()
 
 module.exports = new pg.Pool({
   connectionString: process.env.DB_URL,
-  // ssl: {rejectUnauthorized: false}
+  ssl: {rejectUnauthorized: false}
   // ssl: false
 })
