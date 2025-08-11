6a5b9b1f9295028a6633e6d2fe95c505bea2fcb4
kyle-barco <kylebarco04@gmail.com>
2025-08-11 14:47:35 +0800

    update

--- src/01_models/pool.cjs
+++ src/01_models/pool.cjs
@@ -2,8 +2,8 @@ const pg = require("pg")
 require("dotenv").config()
 
 module.exports = new pg.Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: {rejectUnauthorized: false}
 })
-console.log("Using DB URL:", process.env.DB_URL);
+// console.log("Using DB URL:", process.env.DB_URL);
 // console.log("SSL Config:", { rejectUnauthorized: false });
