6bc0ac0ee1e5457c99f30e64b2b9b11adacff836
kyle-barco <kylebarco04@gmail.com>
2025-08-12 12:25:02 +0800

    ulet

--- src/01_models/pool.cjs
+++ src/01_models/pool.cjs
@@ -1,13 +1,32 @@
 const pg = require("pg");
 require("dotenv").config();
 
-const caCert = process.env.CA.replace(/\\n/g, '\n'); 
+const aivenCA = `-----BEGIN CERTIFICATE-----
+MIIDQTCCAimgAwIBAgITBmyfz5m/jAo54vB4ikPmljZbyjANBgkqhkiG9w0BAQsF
+ADA5MQswCQYDVQQGEwJVUzEPMA0GA1UEChMGQW1hem9uMRkwFwYDVQQDExBBbWF6
+b24gUm9vdCBDQSAxMB4XDTE1MDUyNjAwMDAwMFoXDTM4MDExNzAwMDAwMFowOTEL
+MAkGA1UEBhMCVVMxDzANBgNVBAoTBkFtYXpvbjEZMBcGA1UEAxMQQW1hem9uIFJv
+b3QgQ0EgMTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBALJ4gHHKeNXj
+ca9HgFB0fW7Y14h29Jlo91ghYPl0hAEvrAIthtOgQ3pOsqTQNroBvo3bSMgHFzZM
+9O6II8c+6zf1tRn4SWiw3te5djgdYZ6k/oI2peVKVuRF4fn9tBb6dNqcmzU5L/qw
+IFAGbHrQgLKm+a/sRxmPUDgH3KKHOVj4utWp+UhnMJbulHheb4mjUcAwhmahRWa6
+VOujw5H5SNz/0egwLX0tdHA114gk957EWW67c4cX8jJGKLhD+rcdqsq08p8kDi1L
+93FcXmn/6pUCyziKrlA4b9v7LWIbxcceVOF34GfID5yHI9Y/QCB/IIDEgEw+OyQm
+jgSubJrIqg0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNVHQ8BAf8EBAMC
+AYYwHQYDVR0OBBYEFIQYzIU07LwMlJQuCFmcx7IQTgoIMA0GCSqGSIb3DQEBCwUA
+A4IBAQCY8jdaQZChGsV2USggNiMOruYou6r4lK5IpDB/G/wkjUu0yKGX9rbxenDI
+U5PMCCjjmCXPI6T53iHTAgIUAhX1peFY5RS+6bCtXZPrV3B9+UzSMzEi+skC6b1Y
+6f6WmiZqiyH3mcnL6tj3L6KY37vV96kLo1ZUDUaj4hZjgE/9qRKn6O9VWk+hcD3Y
+d7q5X0Wq9J2+Z5KxsKAvXwfq+JzQdFZClNFAjO2RhuHZ/Y9+zBQK9N4Kkn8QkXzO
+lPqYI4lX5jw8OJ3FQj9NmKuqHs2aQMg35R1fBlv1CAKl+L1p06XPKu1pAmLg6xVG
+Y89MZ7X9QjYj7FhBfJN4p
+-----END CERTIFICATE-----`;
 
 module.exports = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     require: true,
-    rejectUnauthorized: true, // Now properly validating
-    ca: caCert 
+    rejectUnauthorized: true,
+    ca: aivenCA
   }
 });
