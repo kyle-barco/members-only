5707dbfa7cd161bdff1ef185ba448bd4aa9b2d25
kyle-barco <kylebarco04@gmail.com>
2025-08-13 08:54:32 +0800

    move to koyeb db

--- src/01_models/pool.cjs
+++ src/01_models/pool.cjs
@@ -5,9 +5,6 @@ require("dotenv").config()
 module.exports = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    // ca: fs.readFileSync('../../ca.pem').toString()
     rejectUnauthorized: false
   }
 })
-// console.log("Using DB URL:", process.env.DB_URL);
-// console.log("SSL Config:", { rejectUnauthorized: false });
