3c450145bff723f043f8479c72ad9521fdae2bbd
kyle-barco <kylebarco04@gmail.com>
2025-08-13 08:45:10 +0800

    trying to update

--- src/01_models/pool.cjs
+++ src/01_models/pool.cjs
@@ -5,8 +5,8 @@ require("dotenv").config()
 module.exports = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    ca: fs.readFileSync('../../ca.pem').toString()
-    // rejectUnauthorized: false
+    // ca: fs.readFileSync('../../ca.pem').toString()
+    rejectUnauthorized: false
   }
 })
 // console.log("Using DB URL:", process.env.DB_URL);
